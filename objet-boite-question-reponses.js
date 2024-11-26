12f75b27ca07f5d5ee8038198aeef897b0948882
Clarelle974 <clarelle.lemonnier@gmail.com>
2024-11-26 16:46:54 +0100

    ajout de fichiers js comme proposition de décomposition des tâches, et proposition de tâches

---
+++ objet-boite-question-reponses.js
@@ -0,0 +1,10 @@
+let boiteAQuestions1 = {
+    const bouton1=//récupérer le bouton 1 dans le HTML
+    const bouton2=//etc...
+    question = "Quel est ta ville ?",
+    reponsesPossibles = ["réponse 1", "réponse 2", "réponse 3", "réponse 4"]
+    reponseJuste = bouton2,
+    //reponseUtilisateur = addevenlistener sur le bouton2
+    //fontion : mettre une fonction qui récupère le bouton de réponse juste 
+    //et ajoute des points au compteur si on clique dessus
+}
---
+++ popup.js
@@ -0,0 +1,2 @@
+//créer ce qui permet d'afficher l'objet boite a question et le faire disparaitre ensuite, 
+//sans remettre à zéro le compteur
