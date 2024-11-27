86e24f698f832ad8623f25a412a3d1e3ffed01c2
Clarelle974 <clarelle.lemonnier@gmail.com>
2024-11-27 23:44:17 +0100

    debug du fichier apparitionDivQuestions + intégration des bons noms de classe

--- apparitionDivQuestions.js
+++ apparitionDivQuestions.js
@@ -1,14 +1,19 @@
 // rédaction de : "Quand je clique sur un bloc pays, 
 //tous les blocs pays disparaissent et faire apparaitre un bloc de question"
+
+
+// déclaration d'une fonction 
 function apparitionDivQuestions() {
-    const blocPays = document.querySelectorAll(".bloc-pays");//prendre le bon nom de class
+    const blocPays = document.querySelector(".container");//prendre le bon nom de class
     // je récupère le bloc qui contient les pays 
-    
-    blocPays.addEventListener ('click', function () {
+    console.log(blocPays);
+    blocPays.addEventListener('click', function () {
         // j'écoute les événements sur blocPays
-        blocPays.setAttribute("display:none");// je fais disparaitre mon bloc Pays en modifiant uniquement la ligne dislay de son css
-        
-        const blocQuestions = document.querySelectorAll('bloc-questions');//prendre le bon nom de class
-        blocQuestions.setAttribute("display:flex");//je rends visible le blocQuestions en modifiant uniquement la ligne display de son CSS
-    });
-    };
+       blocPays.style.display = 'none';// je fais disparaitre mon bloc Pays en modifiant uniquement la ligne dislay de son css
+        const blocQuestions = document.querySelector(".Quizz");//prendre le bon nom de class
+        blocQuestions.style.display = 'flex';//je rends visible le blocQuestions en modifiant uniquement la ligne display de son CSS
+        });
+};
+
+// appel de la fonction 
+apparitionDivQuestions();
