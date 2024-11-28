fb177b35cc003402113d46845080995ff611963b
Kevin <kg71875@gmail.com>
2024-11-28 16:00:56 +0100

    recuperation du main sur ma branche

---
+++ script.js
@@ -0,0 +1,19 @@
+// Quand tu clique sur ton boutton account, tu dois écrire dans un prompt ton pseudo
+
+// const accountButton = document.querySelector(".button");
+// accountButton.addEventListener("click"(prompt("Entre ton pseudo")));
+// console.log(accountButton);
+
+// const accountButton = document.querySelector(".button");
+// accountButton.addEventListener("click", function () {
+//   userName = prompt("Entre ton pseudo");
+//   console.log(userName);
+// });
+
+const accountButton = document.querySelector(".account");
+accountButton.addEventListener("click", function () {
+  userName = prompt("Entre ton pseudo");
+  console.log(userName);
+});
+
+//clic sur une classe continent
