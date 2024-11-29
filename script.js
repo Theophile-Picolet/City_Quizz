75a1b81f9230de0d557009f4e4be5e0e7aa24870
Clarelle974 <clarelle.lemonnier@gmail.com>
2024-11-29 15:59:44 +0100

    modifs du script : affichage des questions et reponses et de l'html

--- script.js
+++ script.js
@@ -2,7 +2,35 @@ const accountButton = document.querySelector(".account");
 const welcome = document.querySelector(".user");
 const quiz = document.querySelector(".Quizz");
 const qE = document.querySelector(".qE");
-const qAf = document.querySelector(".qE");
+
+const main = document.querySelector(".main");
+const europe = document.querySelector(".europe");
+const jeu = document.querySelector(".jeu")
+let questionToDisplay = 0;
+
+// faire apparaitre et disparaitre les blocs
+europe.addEventListener("click", function () {
+  main.style.display = "none";
+  quiz.style.display = "flex";
+  qE.textContent = questionsEurope[questionToDisplay].question;
+  const reponsesDiv = document.createElement("div");
+  reponsesDiv.innerHTML = "";
+  jeu.appendChild(reponsesDiv);
+  console.log(reponsesDiv);
+
+  const boutonReponse = document.createElement("button");
+  reponsesDiv.appendChild(boutonReponse);
+  
+  questionsEurope[questionToDisplay].reponses.forEach((reponse) => {
+    const button = document.createElement("button");
+    button.textContent = reponse ;
+    button.classList.add("qE");
+    reponsesDiv.appendChild(button);
+    console.log("button"+button);
+  //for (i=0; i<reponsesEurope.length; i++) {
+  //reponsesEurope[0].textContent = questionsEurope[0].reponses[0];
+  })}
+);
 
 //Fonction timer ;
 function timer () { 
@@ -16,90 +44,106 @@ function timer () {
     }, 1000)};
     timer();
 
-// ".question".classList.add (".europe");
-
-// ajouter const quiz1, quiz2 ...
-accountButton.addEventListener("click", function () {
-  userName = prompt("Entre ton pseudo");
-  welcome.textContent = userName ? `Bienvenue ${userName} !!` : `Bienvenue !!`;
-});
-
 // création d'un tableau d'objets contenant les questions et les réponses pour le bloc Europe
 const questionsEurope = [
-    {
-        question : "Quelle expression est consacrée par le traité de Maastricht ?",
-        reponse1 :"la culture eurasiatique", 
-        reponse2 : "la diversité culturelle", 
-        reponse3 : "la culture générale", 
-        reponse4 : "les droits de l'Homme",
-        reponseCorrecte : "la diversité culturelle"
-    },
-    {
-        question : "l'Union europééenne mène des actions militaires ... :",
-        _reponses : ["au Pakistan ?","en Macédoine du Nord ?","en Ukraine ?","en Bosnie ?"],
-        get reponses(){
-            return this._reponses;
-        },
-        reponseCorrecte() {
-            return this.reponses[2];//attention : distinguer le numéro de la réponse et son index
-        },
-    },
-    {
-        question : "Quelle est la personne la plus riche d'Europe ?",
-        _reponses : ["Volodymyr Zelenski","Bernard Arnault","Emmanuel Macron","JuL"],
-        get reponses(){
-            return this._reponses;
-        },
-        reponseCorrecte() {
-            return this.reponses[1];//attention : distinguer le numéro de la réponse et son index
-        }
-    }
+  {
+      question : "Quelle expression est consacrée par le traité de Maastricht ?",
+      reponses : ["la culture eurasiatique", "la diversité culturelle", "la culture générale", "les droits de l'Homme"],
+      reponseCorrecte : "la diversité culturelle",
+  },
+  {
+      question : "l'Union europééenne mène des actio4ns militaires ... :",
+      reponses : ["au Pakistan ?", "en Macédoine du Nord ?", "en Ukraine ?", "en Bosnie ?"],
+      reponseCorrecte : "en Ukraine ?",
+      },
+  {
+      question : "Quelle est la personne la plus riche d'Europe ?",
+      reponses :["Volodymyr Zelenski", "Bernard Arnault", "Emmanuel Macron", "JuL"],
+      reponseCorrecte : "Bernard Arnault",
+  }
 ];
 
-//création d'une fonction random pour afficher les questions dans un ordre aléatoire 
+// function displayQuestion() {
+//     const questionDiv = document.querySelector("div");
+//     questionDiv.innerHTML = "";
 
-// partie sur les pays
-const main = document.querySelector(".main");
-const europe = document.querySelector(".europe");
-const reponsesEurope = [document.querySelectorAll(".qE.button")]
-console.log(reponsesEurope);
+//     const questionAffichee = document.createElement("p");
+//     questionAffichee.classList.add("qE");
+//     questionAffichee.innerHTML = <p>questionsEurope[questionToDisplay].question</p>;
+//     console.log(questionAffichee);
+//     questionDiv.appendChild(questionAffichee);
+
+    // création du H2
+    // const question = document.createElement("h2");
+    // question.innerText = questionsEurope[questiontoDisplay].question;
+    // card.appendChild(question);
+
+    //création du bouton
+    // const reponsesDiv = document.createElement("div");
+    // reponsesDiv.innerHTML = "";
+    // questionsEurope[questionToDisplay].reponses.forEach((reponse) => {
+    //     const button = document.createElement("button");
+    //     button.textContent = reponse ;
+    //     button.classList.add("qE");
+    //     reponsesDiv.appendChild(button);
+    //     console.log("button"+button);
+        // button.addEventListener("click", () => {
+        //     questionToDisplay++
+        //     if (questionToDisplay > questionsEurope.length -1) {
+        //         console.log("fin du jeu")
+        //     }
+        //     else {
+        //         displayQuestion();
+        //     }
+           // console.log(button.textContent);
+            // if (button.textContent === questionsEurope[questionToDisplay].correctAnswer) {
+            //     console.log("bonne réponse");
+            //   } else {
+            //     console.log("mauvaise réponse");
+            //   }
+          // }
+  //       )}
+  // displayQuestion();
+      //)
+    //}
+    
+
+        
+              
+              // questionToDisplay++;
+           // score incrémenté
+           
+  
+
+
+// const reponsesEurope = [document.querySelectorAll(".qE.button")]
+// console.log(reponsesEurope);
 //console.log("reponsesEurope[1]"+reponsesEurope[1]);
 //let bouton =0;
 //for ( i=0; i<reponsesEurope.length; i++) {
 //  bouton[i] = reponsesEurope[i];
 //  console.log("bouton[i]" +bouton[i])};
-
-europe.addEventListener("click", function () {
-  main.style.display = "none";
-  quiz.style.display = "flex";
-  qE.textContent = questionsEurope[0].question;
-  //for (i=0; i<reponsesEurope.length; i++) {
-  //reponsesEurope[0].textContent = questionsEurope[0].reponses[0];
-  }
-  
-//}
-);
-const asia = document.querySelector(".asia");
-asia.addEventListener("click", function () {
-  main.style.display = "none";
-  quiz.style.display = "flex";
-  //   changer le quiz en quiz2
-});
-const america = document.querySelector(".america");
-america.addEventListener("click", function () {
-  main.style.display = "none";
-  quiz.style.display = "flex";
-});
-const africa = document.querySelector(".africa");
-africa.addEventListener("click", function () {
-  main.style.display = "none";
-  quiz.style.display = "flex";
-  qE.textContent = "Blablabla";
-});
-const australia = document.querySelector(".australia");
-australia.addEventListener("click", function () {
-  main.style.display = "none";
-  quiz.style.display = "flex";
-});
+// const asia = document.querySelector(".asia");
+// asia.addEventListener("click", function () {
+//   main.style.display = "none";
+//   quiz.style.display = "flex";
+//   //   changer le quiz en quiz2
+// });
+// const america = document.querySelector(".america");
+// america.addEventListener("click", function () {
+//   main.style.display = "none";
+//   quiz.style.display = "flex";
+// });
+// const africa = document.querySelector(".africa");
+// africa.addEventListener("click", function () {
+//   main.style.display = "none";
+//   quiz.style.display = "flex";
+//   qE.textContent = "Blablabla";
+// });
+// const australia = document.querySelector(".australia");
+// australia.addEventListener("click", function () {
+//   main.style.display = "none";
+//   quiz.style.display = "flex";
+// });
 
 
